8d95bac0b79bcf1a030a0e1216ba607e78cb9975
Mickae Truman <mtruman@nyit.edu>
2019-03-25 13:04:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,23 @@
-class Triangle
+def triangle(a, b, c)
   
-def initialize(side_a, side_b, side_c)
-  @side_a = side_a
-  @side_b = side_b
-  @side_c = side_c
-end
-
-def kind(side_a, side_b, side_c)
-  if side_a == side_b && side_b == side_c then
+  s = (a + b + c) / 2.0
+  
+  # the following must be positive to be a valid triangle
+  ok = (s - a) * (s - b) * (s - c)
+  
+  if a <= 0 || b <= 0 || c <= 0 || ok <= 0 then 
+    raise TriangleError
+  end
+  
+  if a == b && b == c then
     :equilateral
-  elsif side_a == side_b || side_a == side_c || side_b == side_c then
+  elsif a == b || a == c || b == c then
     :isosceles
   else
     :scalene
   end
 end
 
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
 end
