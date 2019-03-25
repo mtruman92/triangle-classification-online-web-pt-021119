080afbad53d41b0c7761520e32b6b85e82772824
Mickae Truman <mtruman@nyit.edu>
2019-03-25 13:01:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,14 @@ def initialize(side_a, side_b, side_c)
 end
 
 def kind
+  if side_a == side_b && side_b == side_c then
+    :equilateral
+  elsif side_a == side_b || side_a == side_c || side_b == side_c then
+    :isosceles
+  else
+    :scalene
+  end
+end
 end
 
 end
